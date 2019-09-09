5aa1f01913ce493b568f0cfa737ebb2e04662c2d
Joseph Laurino <joseph.laurino@gmail.com>
2019-09-09 09:44:20 -0700

    add C++ Actor and derived Blueprint

--- Source/QuickStart/FloatingActor.cpp
+++ Source/QuickStart/FloatingActor.cpp
@@ -8,6 +8,17 @@ AFloatingActor::AFloatingActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	VisualMesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+
+	if (CubeVisualAsset.Succeeded())
+	{
+		VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+		VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	}
+
 }
 
 // Called when the game starts or when spawned
@@ -22,5 +33,13 @@ void AFloatingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector NewLocation = GetActorLocation();
+	FRotator NewRotation = GetActorRotation();
+	float RunningTime = GetGameTimeSinceCreation();
+	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+	NewLocation.Z += DeltaHeight * FloatSpeed;          //Scale our height by FloatSpeed
+	float DeltaRotation = DeltaTime * RotationSpeed;    //Rotate by a number of degrees equal to RotationSpeed each second
+	NewRotation.Yaw += DeltaRotation;
+	SetActorLocationAndRotation(NewLocation, NewRotation);
 }
 
